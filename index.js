ca630c40f742599e3267c334aab81154a73c712e
Brian Mutinda <mutindabrian26@gmail.com>
2020-06-18 16:35:16 +0300

    add joi package to validate input

--- index.js
+++ index.js
@@ -22,11 +22,16 @@ app.get('/api/courses/:id', (req, res)=>{
 })
 
 app.post('/api/courses', (req, res) => {
+    if (!req.body.name || req.body.name.length <= 3){
+        // 400 bad request
+        res.status(400).send('Name is required and should be a minimum of 3 characters')
+        return;
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
     };
-
     courses.push(course);
     res.send(course);
 })
