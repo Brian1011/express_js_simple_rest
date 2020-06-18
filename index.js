364d167c2f5a4fee6c249e0a074c65a9ce1e63a2
Brian Mutinda <mutindabrian26@gmail.com>
2020-06-18 16:18:21 +0300

    add params and queries

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const express = require('express')
 const app = express();
 
 app.get('/', (request, response)=>{
-    response.send('Hello World');
+    response.send('Hello World!!!');
 })
 
 app.get('/api/courses', (request, response)=>{
-    response.send([1,2,3])
+    response.send([1,2,3,5])
 })
 
-app.listen(3000, ()=>console.log('Listening on port 3000'))
+app.get('/api/courses/:year/:month', (req, res)=>{
+    res.send(req.query)
+})
+
+// PORT
+const port = process.env.PORT || 3000
+app.listen(3000, ()=>console.log(`Listening on port ${port}`))
