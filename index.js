89b2e8f0ccb309f1e3a6d1315127583566324faa
Brian Mutinda <mutindabrian26@gmail.com>
2020-06-18 19:14:27 +0300

    retructure code

--- index.js
+++ index.js
@@ -19,16 +19,14 @@ app.get('/api/courses', (request, response)=>{
 
 app.get('/api/courses/:id', (req, res)=>{
     let course = courses.find(c=>c.id === parseInt(req.params.id))
-    if(!course) res.status(404).send('The course with the given ID was not found')
+    if(!course) return res.status(404).send('The course with the given ID was not found')
     res.send(course)
 })
 
 app.post('/api/courses', (req, res) => {
     const {error} = validateCourse(req.body); // object destructuring
-    if(error){
-        res.status(400).send(error.details)
-        return
-    }
+    if(error)return res.status(400).send(error.details)
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -40,7 +38,7 @@ app.post('/api/courses', (req, res) => {
 app.put('/api/courses/:id', (req, res) => {
     // Look up the course
     let course = courses.find(c=>c.id === parseInt(req.params.id))
-    if(!course) res.status(404).send('The course with the given ID was not found')
+    if(!course) return res.status(404).send('The course with the given ID was not found')
 
     const result = validateCourse(req.body);
     const {error} = validateCourse(req.body); // object destructuring
