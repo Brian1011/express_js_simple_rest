99228e48b30c2de2d0ab85d2288f36500a69970f
Brian Mutinda <mutindabrian26@gmail.com>
2020-06-18 16:30:17 +0300

    post request

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express')
 const app = express();
+app.use(express.json());
 
+const courses = [
+    {id:1, name:'Course 1'},
+    {id:2, name:'Course 2'},
+    {id:3, name:'Course 3'}
+]
 app.get('/', (request, response)=>{
     response.send('Hello World!!!');
 })
 
 app.get('/api/courses', (request, response)=>{
-    response.send([1,2,3,5])
+    response.send(courses)
 })
 
-app.get('/api/courses/:year/:month', (req, res)=>{
-    res.send(req.query)
+app.get('/api/courses/:id', (req, res)=>{
+    let course = courses.find(c=>c.id === parseInt(req.params.id))
+    if(!course) res.status(404).send('The course with the given ID was not found')
+    res.send(course)
+})
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+
+    courses.push(course);
+    res.send(course);
 })
 
 // PORT
